e1c98db9283edd8dc73c10ce17cdcdf49fbcd250
deepakmench18 <deepakmench18@gmail.com>
2021-06-03 14:40:20 +0530

    Show if its a win or tie

--- Flip_simulator.sh
+++ Flip_simulator.sh
@@ -1,18 +1,16 @@
-heads=1
-tails=0
-headswin=0
-tailswin=0
-echo "enter n  how many time you wantu to loop through"
-read n
-for (( i=0;i<$n;i++ ))
-do
-   toss=$((RANDOM%2))
-   if [ $toss -eq $heads ]
-   then
-      ((headswin++))
-   else
+ else
       ((tailswin++))
    fi
+   if [ $headswin -eq $tailswin ]
+   then
+      echo "It's a tie "
+      break
+   fi
 done
-echo "Heads win $headswin time"
-echo "Tails win $tailswin time"
+if [ $headswin -eq $tailswin ]
+then
+   echo "It's a tie "
+fi
+echo "Heads win $headswin times"
+echo "Tails win $tailswin times"
+
