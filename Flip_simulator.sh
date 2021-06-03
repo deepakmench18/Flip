706a1dd9fc146938be2e0271f56683f78b8fd5e0
deepakmench18 <deepakmench18@gmail.com>
2021-06-03 14:47:39 +0530

    Tie then game continues till the mini 2 point

--- Flip_simulator.sh
+++ Flip_simulator.sh
@@ -4,13 +4,16 @@
    if [ $headswin -eq $tailswin ]
    then
       echo "It's a tie "
+      while [ $(($headswin-$tailswin)) -le 2 ]
+      do
+         toss=$((RANDOM%2))
+         if [ $toss -eq $heads ]
+         then
+            ((headswin++))
+         else
+            ((tailswin++))
+         fi
+      done
       break
-   fi
+      fi
 done
-if [ $headswin -eq $tailswin ]
-then
-   echo "It's a tie "
-fi
-echo "Heads win $headswin times"
-echo "Tails win $tailswin times"
-
