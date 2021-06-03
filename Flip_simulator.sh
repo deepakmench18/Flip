a9ed10418485288b3d095e9867463dc3f2773ed1
deepakmench18 <deepakmench18@gmail.com>
2021-06-03 14:29:53 +0530

    multiple times of flipping a coin check head or tail won

--- Flip_simulator.sh
+++ Flip_simulator.sh
@@ -1,9 +1,18 @@
 heads=1
 tails=0
-toss=$((RANDOM%2))
-if [ $toss -eq $heads ]
-then
-   echo "Heads win"
-else
-   echo "Tails win "
-fi
+headswin=0
+tailswin=0
+echo "enter n  how many time you wantu to loop through"
+read n
+for (( i=0;i<$n;i++ ))
+do
+   toss=$((RANDOM%2))
+   if [ $toss -eq $heads ]
+   then
+      ((headswin++))
+   else
+      ((tailswin++))
+   fi
+done
+echo "Heads win $headswin time"
+echo "Tails win $tailswin time"
